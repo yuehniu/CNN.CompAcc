105d1e942afba24180140f170dd85c0284e5f16a
Yue-Niu <yue.niu@foxmail.com>
2017-11-01 16:27:01 +0800

    Modify: Add top-5 accuracy information.

--- torch.overfeat/testOverfeat.lua
+++ torch.overfeat/testOverfeat.lua
@@ -11,7 +11,7 @@ testLogger = io.open(paths.concat(opt.save, 'test.log'), 'w+')
 runLogger = io.open(paths.concat(opt.save, 'run.log'), 'w+')
 
 local batchNumber
-local top1_center, loss
+local top1_center, loss, top5_center
 local timer = torch.Timer()
 
 function test()
@@ -27,6 +27,7 @@ function test()
    model:evaluate()
 
    top1_center = 0
+   top5_center = 0
    loss = 0
    for i = 1, math.ceil(nTest/opt.batchSize) do -- nTest is set in 1_data.lua
       local indexStart = (i-1) * opt.batchSize + 1
@@ -46,6 +47,7 @@ function test()
    cutorch.synchronize()
 
    top1_center = top1_center * 100 / nTest
+   top5_center = top5_center * 100 / nTest
    loss = loss / nTest -- because loss is calculated per batch
    -- testLogger:add{
    --    ['% top1 accuracy (test set) (center crop)'] = top1_center,
@@ -56,11 +58,20 @@ function test()
                           .. 'Top-1(%%):\t %.2f\t ',
                        layer_, epoch, rank_, timer:time().real, loss, top1_center))
    testLogger:write('\n')
+   testLogger:write(string.format('ConvLayer: [%d], Epoch: [%d], Rank: [%d], Total Time(s): %.2f \t\t'
+                          .. 'average loss: %.2f \t '
+                          .. 'Top-5(%%):\t %.2f\t ',
+                       layer_, epoch, rank_, timer:time().real, loss, top5_center))
+   testLogger:write('\n')
    testLogger:flush()
    runLogger:write(string.format('ConvLayer: [%d], Epoch: [%d], Rank: [%d], Total Time(s): %.2f \t\t'
                           .. 'average loss: %.2f \t '
                           .. 'Top-1(%%):\t %.2f\t ',
                        layer_, epoch, rank_, timer:time().real, loss, top1_center))
+   runLogger:write(string.format('ConvLayer: [%d], Epoch: [%d], Rank: [%d], Total Time(s): %.2f \t\t'
+                          .. 'average loss: %.2f \t '
+                          .. 'Top-5(%%):\t %.2f\t ',
+                       layer_, epoch, rank_, timer:time().real, loss, top5_center))
    runLogger:write('\n\n')
    runLogger:flush()
 
@@ -69,6 +80,11 @@ function test()
                           .. 'accuracy [Center](%%):\t top-1 %.2f\t ',
                        epoch, timer:time().real, loss, top1_center))
 
+   print('\n')
+   print(string.format('Epoch: [%d][TESTING SUMMARY] Total Time(s): %.2f \t'
+                          .. 'average loss (per batch): %.2f \t '
+                          .. 'accuracy [Center](%%):\t top-5 %.2f\t ',
+                       epoch, timer:time().real, loss, top5_center))
    print('\n')
 
 
@@ -94,10 +110,16 @@ function testBatch(inputsCPU, labelsCPU)
    for i=1,pred:size(1) do
       local g = labelsCPU[i]
       if pred_sorted[i][1] == g then top1_center = top1_center + 1 end
+      if pred_sorted[i][1] == g or pred_sorted[i][2] == g or pred_sorted[i][3] == g or pred_sorted[i][4] == g or pred_sorted[i][5] == g then 
+        top5_center = top5_center + 1 
+      end 
    end
    if batchNumber % 1024 == 0 then
       print(('Epoch: Testing [%d], Loss: [%.2f], Accuracy: [%.2f](%%)----[%d/%d]'):format(epoch, loss/batchNumber, top1_center*100/batchNumber, batchNumber, nTest))
       runLogger:write(('Epoch: Testing [%d], Loss: [%.2f], Accuracy: [%.2f](%%)----[%d/%d]'):format(epoch, loss/batchNumber, top1_center*100/batchNumber, batchNumber, nTest))
+      runLogger:write('\n')
+      print(('Epoch: Testing [%d], Loss: [%.2f], Accuracy-5: [%.2f](%%)----[%d/%d]'):format(epoch, loss/batchNumber, top5_center*100/batchNumber, batchNumber, nTest))
+      runLogger:write(('Epoch: Testing [%d], Loss: [%.2f], Accuracy-5: [%.2f](%%)----[%d/%d]'):format(epoch, loss/batchNumber, top5_center*100/batchNumber, batchNumber, nTest))
       runLogger:write('\n')
       runLogger:flush()
    end
