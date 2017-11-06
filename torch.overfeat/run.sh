0bafba19866f53e4d852b5f14b3ec0d54cd58956
Yue-Niu <yue.niu@foxmail.com>
2017-11-06 17:02:41 +0800

    Modify: Shell run.

--- torch.overfeat/run.sh
+++ torch.overfeat/run.sh
@@ -1,1 +1,14 @@
-th main.lua -data /data/niuy/ml.DataSet/ILSVRC2012/ -netType overfeat -cropSize 221 -nEpochs 80
+# Finetune for big original Overfeat
+th mainTrain.lua 
+
+# Big original model for Overfeat
+# th mainTest.lua -data /data/niuy/ml.DataSet/ILSVRC2012/ -netType overfeat -cropSize 221 -nEpochs 1 -retrain model.t7
+
+# Big finetune model for Overfeat
+# th mainTest.lua -data /data/niuy/ml.DataSet/ILSVRC2012/ -netType overfeat -cropSize 221 -nEpochs 1 -retrain imagenet/checkpoint/overfeat/finetuneOverfeatThread-b-conv1-wellSolver/model_5.t7
+
+# Small model for Overfeat
+# th mainTest.lua -data /data/niuy/ml.DataSet/ILSVRC2012/ -netType overfeat -cropSize 231 -nEpochs 1 -retrain model.small.t7 
+
+# Big model for Overfeat in FCNN
+# th mainTest.lua -data /data/niuy/ml.DataSet/ILSVRC2012/ -netType overfeat -cropSize 256 -nEpochs 1 
